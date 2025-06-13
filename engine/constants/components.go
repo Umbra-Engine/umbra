1514584ca4ed5093742bb20617ba16866f69f6f0
Sp0k <contact@gabsavard.com>
2025-06-13 19:43:02 -0300

    feat(components): added a rigidbody component
    - Created the rigid body component and its constants
    - Updated the vector helpers with a set of operations for `Vector2`,
      `Vector3` and `Vector4`

--- engine/constants/components.go
+++ engine/constants/components.go
@@ -6,15 +6,18 @@ const (
 	ComponentCamera         = "Camera"
 	ComponentTransform      = "Transform"
 	ComponentScript         = "Script"
+	ComponentRigidBody      = "RigidBody"
 )
 
 // Common Component Field Keys
 const (
+	// Shared fields
 	FieldActive = "active"
+	FieldLayer  = "layer"
+	FieldMask   = "mask"
 
-	// SpriteRenderer fields
+	// Sprite Renderer fields
 	FieldTexture = "texture"
-	FieldLayer   = "layer"
 	FieldFOV     = "fov"
 
 	// Transform fields
@@ -31,4 +34,17 @@ const (
 	FieldEnabled   = "enabled"
 	FieldClassName = "classname"
 	FieldParams    = "params"
+
+	// Rigid Body fields
+	FieldMass            = "mass"
+	FieldVelocity        = "velocity"
+	FieldAngularVelocity = "angular_velocity"
+	FieldUseGravity      = "use_gravity"
+	FieldIsKinematic     = "is_kinematic"
+	FieldLinearDamp      = "linear_damp"
+	FieldAngularDamp     = "angular_damp"
+	FieldForce           = "force"
+	FieldTorque          = "torque"
+	FieldFreezePosition  = "freeze_position"
+	FieldFreezeRotation  = "freeze_rotation"
 )
