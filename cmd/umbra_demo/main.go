ec61d2c70cb90b98ef1320795b105e4b020ec004
Sp0k <contact@gabsavard.com>
2025-06-17 12:22:14 -0300

    dependency: replaced Ebiten for OpenGL
    - Removed Ebiten dependency from the project
    - Started to build the graphics pipeline with OpenGL and GLFW

--- cmd/umbra_demo/main.go
+++ cmd/umbra_demo/main.go
@@ -1,38 +1,72 @@
 package main
 
 import (
-	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
-	"log"
-
 	"github.com/Umbra-Engine/umbra/engine/core"
+	"github.com/Umbra-Engine/umbra/engine/logger"
+	"github.com/go-gl/gl/v4.1-core/gl"
+	"github.com/go-gl/glfw/v3.3/glfw"
+	"runtime"
 )
 
 type Game struct{}
 
-func (g *Game) Update() error {
-	return nil
-}
-
-func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Umbra Engine - Running")
-}
-
-func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
-	return 640, 480
-}
-
 func main() {
+	runtime.LockOSThread() // Ensures all GLFW call runs on main OS thread (macOS requires this)
+
+	if err := glfw.Init(); err != nil {
+		logger.Fatal("failed to initialize GLFW: %v", err)
+	}
+	defer glfw.Terminate()
+
 	config, err := core.LoadConfig("config.yaml")
 	if err != nil {
-		log.Fatalf("failed to load config: %v", err)
+		logger.Fatal("failed to log config: %v", err)
 	}
 
-	ebiten.SetWindowSize(config.Window.Width, config.Window.Height)
-	ebiten.SetWindowTitle(config.Window.Title)
-	ebiten.SetFullscreen(config.Window.Fullscreen)
+	initWindow()
+	window := createWindow(config.Window)
+	initGL(config.Window)
 
-	if err := ebiten.RunGame(&Game{}); err != nil {
-		log.Fatal(err)
+	gameLoop(window)
+	window.Destroy()
+}
+
+func initWindow() {
+	glfw.WindowHint(glfw.ContextVersionMajor, 4)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+}
+
+func createWindow(windowConfig core.WindowConfig) *glfw.Window {
+	window, err := glfw.CreateWindow(windowConfig.Width, windowConfig.Height, windowConfig.Title, nil, nil)
+	if err != nil {
+		logger.Fatal("failed to create the %s game window", windowConfig.Title)
+	}
+	window.MakeContextCurrent()
+	return window
+}
+
+func initGL(windowConfig core.WindowConfig) {
+	if err := gl.Init(); err != nil {
+		logger.Fatal("failed to initialize OpenGL: %v", err)
+	}
+	gl.Viewport(0, 0, int32(windowConfig.Width), int32(windowConfig.Height))
+	gl.ClearColor(
+		float32(windowConfig.BackgroundColor.X),
+		float32(windowConfig.BackgroundColor.Y),
+		float32(windowConfig.BackgroundColor.Z),
+		float32(windowConfig.BackgroundColor.W),
+	)
+}
+
+func gameLoop(window *glfw.Window) {
+	for !window.ShouldClose() {
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+		// TODO: Add Umbra-specific rendering logic here
+
+		window.SwapBuffers()
+		glfw.PollEvents()
 	}
 }
--- go.mod
+++ go.mod
@@ -4,18 +4,14 @@ go 1.24.4
 
 require (
 	github.com/fatih/color v1.18.0
-	github.com/hajimehoshi/ebiten/v2 v2.8.8
+	github.com/go-gl/gl v0.0.0-20231021071112-07e5d0ea2e71
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20250301202403-da16c1255728
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
-	github.com/ebitengine/gomobile v0.0.0-20240911145611-4856209ac325 // indirect
-	github.com/ebitengine/hideconsole v1.0.0 // indirect
-	github.com/ebitengine/purego v0.8.0 // indirect
-	github.com/jezek/xgb v1.1.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
 )
 
